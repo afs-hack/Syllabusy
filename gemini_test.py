ed2bb46c0bb25d662def5cfb6b35e09801430353
Arvind Sundar <arvindsundar.hack@gmail.com>
2025-10-04 16:26:05 -0500

    Add gemini pdf read test

--- gemini_test.py
+++ gemini_test.py
@@ -1,13 +1,29 @@
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
+import pathlib
+import os
+
+# .env file contains the GEMINI_API_KEY
+# and TEST_PDF_PATH environment variables.
 
 load_dotenv()
+
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-print("hello")
+TEST_PDF_PATH = os.getenv("TEST_PDF_PATH")
 
+print("Beginning PDF read test...")
+
+filepath = pathlib.Path(TEST_PDF_PATH).expanduser()
+
+prompt = "who is the professor and where (and when) is the class held?"
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
+    model="gemini-2.5-flash", contents=[
+      types.Part.from_bytes(
+        data=filepath.read_bytes(),
+        mime_type='application/pdf',
+      ),
+      prompt])
 print(response.text)
